9cb017caf8ac3256133e6f362cf3d4339d764b3a
TOMAS CONESA <TOTOOOC@GMAIL.COM>
2023-09-11 17:34:31 -0300

    Implementacion de form

--- src/Components/Common/Form.jsx
+++ src/Components/Common/Form.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  
+  const onChangeName = (e) => setName(e.target.value);
+  const onChangeEmail = (e) => setEmail(e.target.value);
+
+  const handlerForm = (e) => {
+    e.preventDefault();
+  }
+
+  if(name.length < 5 || !/^\S+@\S+\.\S+$/.test(email)){
+    alert("Por favor verifique su información nuevamente")
+  }
+
   return (
     <div>
-      <form>
+      {submitted ? (<p>Gracias {name}, te contactaremos cuando antes vía mail”</p>) : (
+      <form onSubmit={handlerForm}>
+        <input type="text" placeholder="Nombre de usuario" value={name} onChange={onChangeName} />
+        <input type="email" placeholder="Ingresar email" value={email} onChange={onChangeEmail} />
+
+        <button type="submit">Enviar</button>
       </form>
+      )}
     </div>
   );
 };
