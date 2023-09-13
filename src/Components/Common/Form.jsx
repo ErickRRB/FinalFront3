980a9bb8f1bc6ff4db46822489c8d1904dec894e
ErickRRB <erickricardorb@gmail.com>
2023-09-13 18:54:37 -0300

    Arregladas validaciones de form, faltan estilos

--- src/Components/Common/Form.jsx
+++ src/Components/Common/Form.jsx
@@ -7,27 +7,54 @@ const Form = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [submitted, setSubmitted] = useState(false);
-  
-  const onChangeName = (e) => setName(e.target.value);
-  const onChangeEmail = (e) => setEmail(e.target.value);
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const onChangeName = (e) => {
+    setName(e.target.value);
+    setNameError("");
+  }
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+    setEmailError("");
+  }
 
   const handlerForm = (e) => {
     e.preventDefault();
+    validateForm()
   }
 
-  //if(name.length < 5 || !/^\S+@\S+\.\S+$/.test(email)){
-  //  alert("Por favor verifique su información nuevamente")
-  //}
+  const validateForm = () => {
+    let isValid = true;
+    if (!name) { 
+      setNameError("El nombre es requerido") 
+      isValid = false}
+    if (!email) {
+      setEmailError("El email es requerido")
+      isValid = false
+    }
+    if (name.length < 5) {
+      setNameError("El nombre debe tener al menos 5 caracteres")
+      isValid = false
+    }
+
+    if (isValid) { setSubmitted(true) }
+
+  }
 
   return (
     <div>
-      {submitted ? (<p>Gracias {name}, te contactaremos cuando antes vía mail”</p>) : (
-      <form onSubmit={handlerForm}>
-        <input type="text" placeholder="Nombre de usuario" value={name} onChange={onChangeName} />
-        <input type="email" placeholder="Ingresar email" value={email} onChange={onChangeEmail} />
-
-        <button type="submit">Enviar</button>
-      </form>
+      {submitted ? (<p>Gracias {name}, te contactaremos cuando antes vía mail</p>) : (
+        <form onSubmit={handlerForm}>
+          <div>
+            <input type="text" placeholder="Nombre de usuario" value={name} onChange={onChangeName} />
+            {nameError && <span className="error" style={{ color: "red" }}>{nameError}</span>}
+          </div>
+          <div>
+            <input type="email" placeholder="Ingresar email" value={email} onChange={onChangeEmail} />
+            {emailError && <span className="error" style={{ color: "red" }}>{emailError}</span>}
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
       )}
     </div>
   );
