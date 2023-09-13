98e54e75ab9e00aa98f5ea44fe7a98efc4f427ca
TOMAS CONESA <TOTOOOC@GMAIL.COM>
2023-09-13 17:44:11 -0300

    Implementacion tabla dentistDeail- falta terminar

--- src/Components/DentistDetail/DentistDetail.jsx
+++ src/Components/DentistDetail/DentistDetail.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function DentistDetail({ dentists }) {
+
   const { id } = useParams();
   const dentist = dentists.find(d => d.id === id);
+
+ /*  const getDentist = async()=>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const data = await res.json()
+    
+  }
+
+  useEffect(()=>{
+    getDentist()
+  }) */
+  
+
 
   return (
     <div>
       {/* Mostrar detalles del dentista usando el objeto dentist */}
+      <h2>Detalles del dentista: {dentist.id}</h2>
+      <table>
+        <tr>
+          <th>Nombre</th>
+          <th>Email</th>
+          <th>Número de teléfono</th>
+          <th>Sitio web</th>
+        </tr>
+        <tr>
+          <td>{dentist.name}</td>
+          <td>{dentist.email}</td>
+          <td>{dentist.phone}</td>
+          <td>{dentist.website}</td>
+        </tr>
+      </table>
     </div>
   );
 }
