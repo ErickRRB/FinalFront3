dea2535d766c24b6c9b57293bbdd1db6a0f8df9e
ErickRRB <erickricardorb@gmail.com>
2023-09-13 18:10:55 -0300

    Arreglado el fetch de dentista para detalle de dentista

--- src/Components/DentistDetail/DentistDetail.jsx
+++ src/Components/DentistDetail/DentistDetail.jsx
@@ -1,41 +1,48 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
-function DentistDetail({ dentists }) {
+function DentistDetail() {
 
   const { id } = useParams();
-  const dentist = dentists.find(d => d.id === id);
+  const [dentist, setDentist] = useState({});
+  const navigate = useNavigate();
+  //const { name, email, phone, website } = dentist;
 
- /*  const getDentist = async()=>{
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-    const data = await res.json()
-    
+  const handlerGoBack = () => { navigate(-1) }
+
+  const getDentist = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const data = await response.json();
+    setDentist(data);
   }
 
-  useEffect(()=>{
-    getDentist()
-  }) */
-  
 
+  useEffect(() => {
+    getDentist()
+  })
 
   return (
     <div>
       {/* Mostrar detalles del dentista usando el objeto dentist */}
-      <h2>Detalles del dentista: {dentist.id}</h2>
+      <h2>Detalles del dentista: {id}</h2>
       <table>
-        <tr>
-          <th>Nombre</th>
-          <th>Email</th>
-          <th>Número de teléfono</th>
-          <th>Sitio web</th>
-        </tr>
-        <tr>
-          <td>{dentist.name}</td>
-          <td>{dentist.email}</td>
-          <td>{dentist.phone}</td>
-          <td>{dentist.website}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Número de teléfono</th>
+            <th>Sitio web</th>
+          </tr>
+          <tr>
+            <td>{dentist.name}</td>
+            <td>{dentist.email}</td>
+            <td>{dentist.phone}</td>
+            <td>{dentist.website}</td>
+          </tr>
+        </tbody>
       </table>
+
+      <button onClick={handlerGoBack}>Volver</button>
     </div>
   );
 }
